92afc082e5c2553aee503c7da39da0ee051667ce
unknown <v.sairaghav@gmail.com>
2018-01-15 10:24:27 +0530

    Added Image search and Video search

--- google_search.py
+++ google_search.py
@@ -1,22 +1,73 @@
 import requests,urllib
 from bs4 import BeautifulSoup as BS
 
-def search(search_term):
+def search(search_term,start_page=1,end_page=-1):
 
-    response = requests.get('https://www.google.com/search?q='+search_term+'&start=0')
+    if end_page < start_page:
+        end_page = start_page
 
     result = []
+    
+    while start_page <= end_page:
+        response = requests.get('https://www.google.com/search?q='+search_term+'&start='+str((start_page-1)*10))
 
-    soup = BS(response.text,'html.parser')
+        soup = BS(response.text,'html.parser')
 
-    for links in soup.findAll('a'):
-        if links.has_attr('href'):
-            try:
-                link = links['href'].split('url?q=')[1].split('&sa')[0]
-                if 'webcache' not in link and 'http' in link and not link in result:
-                    result.append(urllib.unquote(link))
-            except:
-                pass
+        for links in soup.findAll('a'):
+            if links.has_attr('href'):
+                try:
+                    link = urllib.unquote(links['href'].split('url?q=')[1].split('&sa')[0])
+                    if 'webcache' not in link and 'http' in link and not link in result:
+                        result.append(link)
+                except:
+                    pass
+
+        start_page += 1
+
+    return result
+
+def search_image(search_term,start_page=1,end_page=-1):
+
+    if end_page < start_page:
+        end_page = start_page
+
+    result = []
+    
+    while start_page <= end_page:
+        response = requests.get('https://www.google.com/search?q='+search_term+'&start='+str((start_page-1)*10)+'&tbm=isch')
+
+        soup = BS(response.text,'html.parser')
+
+        for links in soup.findAll('img'):
+            if links.has_attr('src'):
+                result.append(links['src'])
+
+        start_page += 1
+
+    return result
+
+def search_video(search_term,start_page=1,end_page=-1):
+
+    if end_page < start_page:
+        end_page = start_page
+
+    result = []
+    
+    while start_page <= end_page:
+        response = requests.get('https://www.google.com/search?q='+search_term+'&start='+str((start_page-1)*10)+'&tbm=vid')
+
+        soup = BS(response.text,'html.parser')
+
+        for links in soup.findAll('a'):
+            if links.has_attr('href'):
+                try:
+                    link = urllib.unquote(links['href'].split('url?q=')[1].split('&sa')[0])
+                    if 'webcache' not in link and 'http' in link and not link in result:
+                        result.append(link)
+                except:
+                    pass
+
+        start_page += 1
 
     return result
 
@@ -24,17 +75,11 @@ def search(search_term):
 def get_summary(search_term):
 
     response = requests.get('https://www.google.com/search?q='+search_term+'&start=0')
-
-    results = []
-
+    
     soup = BS(response.text,'html.parser')
 
     for result in soup.findAll('span',{'class':'st'}):
         if not '...' in result.text and not result.text is u'':
-            results.append(result.text)
+            return result.text
 
-    try:
-        return results[0]
-    except:
-        return None
-    
+    return None
